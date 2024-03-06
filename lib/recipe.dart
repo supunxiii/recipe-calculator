68493a5063ba4921dc4fd0767f8cc3cdf0db98fb
supunxiii <supunwijesooriya13@gmail.com>
2024-03-06 22:22:21 +0530

    finished project

---
+++ lib/recipe.dart
@@ -0,0 +1,90 @@
+class Recipe {
+  String label;
+  String imageUrl;
+  int servings;
+  List<Ingredient> ingredients;
+
+
+  Recipe(
+      this.label,
+      this.imageUrl,
+      this.servings,
+      this.ingredients,
+      );
+
+
+  static List<Recipe> samples = [
+    Recipe(
+      'Spaghetti and Meatballs',
+      'assets/spaghetti-meatballs.png',
+      4,
+      [
+        Ingredient(1, 'box', 'Spaghetti',),
+        Ingredient(4, '', 'Frozen Meatballs',),
+        Ingredient(0.5, 'jar', 'sauce',),
+      ],
+    ),
+    Recipe(
+      'Tomato Soup',
+      'assets/tomato-soup.png',
+      2,
+      [
+        Ingredient(1, 'can', 'Tomato Soup',),
+      ],
+    ),
+    Recipe(
+      'Grilled Cheese',
+      'assets/grilled-cheese.png',
+      1,
+      [
+        Ingredient(2, 'slices', 'Cheese',),
+        Ingredient(2, 'slices', 'Bread',),
+      ],
+    ),
+    Recipe(
+      'Chocolate Chip Cookies',
+      'assets/chocolate-chip-cookies.png',
+      24,
+      [
+        Ingredient(4, 'cups', 'flour',),
+        Ingredient(2, 'cups', 'sugar',),
+        Ingredient(0.5, 'cups', 'chocolate chips',),
+      ],
+    ),
+    Recipe(
+      'Taco Salad',
+      'assets/taco-salad.png',
+      1,
+      [
+        Ingredient(4, 'oz', 'nachos',),
+        Ingredient(3, 'oz', 'taco meat',),
+        Ingredient(0.5, 'cup', 'cheese',),
+        Ingredient(0.25, 'cup', 'chopped tomatoes',),
+      ],
+    ),
+    Recipe(
+      'Hawaiian Pizza',
+      'assets/hawaiian-pizza.png',
+      4,
+      [
+        Ingredient(1, 'item', 'pizza',),
+        Ingredient(1, 'cup', 'pineapple',),
+        Ingredient(8, 'oz', 'ham',),
+      ],
+    ),
+  ];
+
+}
+
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(
+      this.quantity,
+      this.measure,
+      this.name,
+      );
+}
+
